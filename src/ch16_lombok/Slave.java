b7bda0283765ad06fc52cdc0b3187b773ea71fb7
yoodeve <janhold@naver.com>
2023-06-30 16:03:01 +0900

    fix: 롬복 코드 수정

--- src/ch16_lombok/Slave.java
+++ src/ch16_lombok/Slave.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -13,7 +13,9 @@ import lombok.ToString;
 // toString오버라이드
 @ToString
 // noArgu생성자도 생성
-@NoArgsConstructor
+//@NoArgsConstructor
+// requiredconstructor => no는 못씀
+@RequiredArgsConstructor
 // setter
 @Setter
 // getter
@@ -22,7 +24,7 @@ import lombok.ToString;
 // 전부다해도라
 @Data
 public class Slave {
-	private String name;
+	private final String name; // final : req에서 필수값처리
 	// 이렇게 하면 name getter는 작성 안됨
 	@Getter
 	private int age;
--- src/ch16_lombok/SlaveMain.java
+++ src/ch16_lombok/SlaveMain.java
@@ -3,7 +3,7 @@ package ch16_lombok;
 public class SlaveMain {
 	public static void main(String[] args) {
 		Slave slave1 = new Slave("한유정", 28);
-		Slave slave2 = new Slave();
+		Slave slave2 = new Slave("nulllll");
 
 		System.out.println(slave1.getAge());
 		System.out.println(slave2.equals(slave1));
