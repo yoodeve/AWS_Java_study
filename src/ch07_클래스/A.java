1f7bcd4f007771853744329323cd6d21a8194763
yoodeve <janhold@naver.com>
2023-07-05 12:30:24 +0900

    chore: 주석 수정

--- src/ch07_클래스/A.java
+++ src/ch07_클래스/A.java
@@ -7,7 +7,6 @@ public class A {
 
 	// 클래스안에는 메소드(클래스안의 함수)도 넣을 수 있다, 자바는 클래스로 만들어지니까 결국 전부 메소드라고 하는게 편함.
 	// void test1(){}
-	// 함수 : x+y = z구할때..처음배움
 	void test1() {
 		System.out.println("test1 함수가 호출됨");
 	}
