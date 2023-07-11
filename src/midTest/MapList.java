ffa87a0448db2f4effa601cc42312c38168b7bb0
yoodeve <janhold@naver.com>
2023-07-11 17:31:46 +0900

    fix: 틀린문제 다시 풀어보기

--- src/midTest/MapList.java
+++ src/midTest/MapList.java
@@ -2,12 +2,12 @@ package midTest;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MapList {
 
 	public static void main(String[] args) {
+
 		Map<String, Object> hong = new HashMap<>();
 		hong.put("name", "홍길동");
 		hong.put("rating", "vip");
@@ -18,12 +18,15 @@ public class MapList {
 		kim.put("rating", "gold");
 		kim.put("age", 35);
 
-		List<Map<String, Object>> customers = new ArrayList<>();
-		customers.add(hong);
-		customers.add(kim);
+		ArrayList<Map<String, Object>> people = new ArrayList<>();
+		people.add(hong);
+		people.add(kim);
 
-		for (String key : hong.keySet()) {
-			System.out.println(key + "=" + hong.get(key));
+		for (Map<String, Object> name : people) {
+			name.entrySet().forEach(ent -> System.out.println(ent));
+			for (Map.Entry<String, Object> entry : name.entrySet()) {
+				System.out.println(entry);
+			}
 		}
 	}
 }
